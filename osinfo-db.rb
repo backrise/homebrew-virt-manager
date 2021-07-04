4b632f548be45fb7ba5f797d01213a869e72f8fa
x <x>
2021-07-04 11:53:35 +0800

    modify url

--- osinfo-db.rb
+++ osinfo-db.rb
@@ -1,7 +1,7 @@
 class OsinfoDb < Formula
   desc "Libosinfo database files"
   homepage "https://libosinfo.org/"
-  url "https://backrise.github.io/libosinfo/osinfo-db-20210426.tar.xz"
+  url "https://backrise.github.io/homebrew-virt-manager/packages/libosinfo/osinfo-db-20210426.tar.xz"
   sha256 "66c93b4a5b319b2ff7d40360d9e94486a9a45f2c0dd1c30d4b31de4f223070d0"
 
   depends_on "osinfo-db-tools" => :build
